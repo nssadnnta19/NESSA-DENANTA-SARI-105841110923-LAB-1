63110c42aeb36a12baef21d889c6ea46049901a0
nssadnnta19 <149997293+nssadnnta19@users.noreply.github.com>
2024-06-03 09:06:58 -0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("iya").innerHTML = "WOWWWWW";
+    document.getElementById("oke").src = "https://th.bing.com/th/id/R.2c38cd299b1c45a9e50dc774daa0bffe?rik=kG6CUuqWBR0XBQ&riu=http%3a%2f%2f4.bp.blogspot.com%2f-3iVX8fZiMAQ%2fUbv_59pEcJI%2fAAAAAAAAAUg%2fE5MK09MjuwY%2fs1600%2fGambar-gambar%2bbunga%2byang%2bindah%2bPaling%2bCantik%2bdipandang%2b3.jpg&ehk=JVbL4cYEf6z8bAYE%2fmJVS%2fj%2frobTNi9GPPri845fhlk%3d&risl=&pid=ImgRaw&r=0";
+
+}
